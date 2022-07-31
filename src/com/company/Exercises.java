c2e0ba5f96b941250e05914c7b7e4e3c6eb5661c
filip šupolík <filipsupolik25@gmail.com>
2022-07-31 23:40:24 +0200

    Complete 3 exercises

--- src/com/company/Exercises.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Exercises {
-    public static double totalDistance(double stepHeight, double stepLength, double towerHeight) {
-       var numberOfSteps = towerHeight/stepHeight;
-       return numberOfSteps * (stepHeight + stepLength);
-    }
-}
